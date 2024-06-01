b722502fc5a7c97e01531a5aff61c6419be57cec
David G. Marchante <Davidgonmar91@gmail.com>
2024-06-01 17:59:58 +0200

    Animation Overhaul

--- Assets/Scripts/Player/InverseKinematics.cs
+++ Assets/Scripts/Player/InverseKinematics.cs
@@ -1,12 +1,8 @@
 using UnityEngine;
-using System;
-using System.Collections;
 
-[ExecuteAlways]
 [RequireComponent(typeof(Animator))]
 public class IKControl : MonoBehaviour
 {
-
     public Animator animator;
 
     public Transform rightHandObj = null;
@@ -15,55 +11,53 @@ public class IKControl : MonoBehaviour
     public Transform leftFootObj = null;
     public Transform lookObj = null;
 
-    void Start()
-    {
-        if(animator == null)
-            animator = GetComponent<Animator>();
-    }
-    void Update()
-    {
-        // Update is called once per frame
-        animator.Update(0);
-    }
-
-    //a callback for calculating IK
-    void OnAnimatorIK()
+    // A callback for calculating IK
+    private void OnAnimatorIK(int layerIndex)
     {
         if (animator)
         {
-            if (lookObj != null)
-            {
-                animator.SetLookAtWeight(1);
-                animator.SetLookAtPosition(lookObj.position);
-            }
-            if (rightHandObj != null)
-            {
-                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
-                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
-                animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
-                animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
-            }
-            if (leftHandObj != null)
-            {
-                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
-                animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
-                animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandObj.position);
-                animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandObj.rotation);
-            }
-            if (rightFootObj != null)
-            {
-                animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1);
-                animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, 1);
-                animator.SetIKPosition(AvatarIKGoal.RightFoot, rightFootObj.position);
-                animator.SetIKRotation(AvatarIKGoal.RightFoot, rightFootObj.rotation);
-            }
-            if (leftFootObj != null)
-            {
-                animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
-                animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1);
-                animator.SetIKPosition(AvatarIKGoal.LeftFoot, leftFootObj.position);
-                animator.SetIKRotation(AvatarIKGoal.LeftFoot, leftFootObj.rotation);
-            }
+            UpdateIKTargets();
+        }
+    }
+
+    private void UpdateIKTargets()
+    {
+        if (lookObj != null)
+        {
+            animator.SetLookAtWeight(1);
+            animator.SetLookAtPosition(lookObj.position);
+        }
+
+        if (rightHandObj != null)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+            animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
+            animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
+        }
+
+        if (leftHandObj != null)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+            animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+            animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandObj.position);
+            animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandObj.rotation);
+        }
+
+        if (rightFootObj != null)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, 1);
+            animator.SetIKPosition(AvatarIKGoal.RightFoot, rightFootObj.position);
+            animator.SetIKRotation(AvatarIKGoal.RightFoot, rightFootObj.rotation);
+        }
+
+        if (leftFootObj != null)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
+            animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1);
+            animator.SetIKPosition(AvatarIKGoal.LeftFoot, leftFootObj.position);
+            animator.SetIKRotation(AvatarIKGoal.LeftFoot, leftFootObj.rotation);
         }
     }
 }
